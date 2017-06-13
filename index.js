a6cb9cd4b3e2118b98ddc118b7e0467b6f38c9c7
Vitalii <overtachyon@gmail.com>
2017-06-13 11:51:06 +0300

    commit22

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-var var1 = "master";
+var var1 = "masterr";
 
 var fortunes = [
     "Победи свои страхи, или они победят тебя.",
